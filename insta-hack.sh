26055013256e56129d0dae12fc1b405ffc359760
sidhaniedotcom <102436523+sidhaniedotcom@users.noreply.github.com>
2022-04-03 01:57:24 +0700

    Update insta-hack.sh

--- insta-hack.sh
+++ insta-hack.sh
@@ -17,7 +17,7 @@ apt-get install python
 apt-get install python2
 clear
 echo -e "$Purple Created By \e[1;34m"
-       figlet Incredible Hacker | lolcat
+       figlet Sidhanie | lolcat
 sleep 2.0
     echo -e " \e[96m  
                                      ████████ \e[0m"
@@ -32,22 +32,22 @@ echo " "
 figlet -f Bloody Insta-Hack | lolcat
 
     echo " "
-    echo -e "$Red                                 ⫸ Coded by$Yellow faizan$Red ⫷\033[0m"
-    echo -e "$Red                               ⫸$Purple Incredible Hackers$Red ⫷\033[0m"
+    echo -e "$Red                    ⫸ Created by$Yellow sIDHANIE$Red ⫷\033[0m"
+    echo -e "$Red                      ⫸$Purple tERMUX sIDHANIE$Red ⫷\033[0m"
 echo " "
 echo -e " $Green     |---------------------------------------------------------------------|"
-echo -e " $Green     ||----------------------------$Cyan [features] $Green---------------------------||"
+echo -e " $Green     ||----------------------$Cyan [features] $Green----------------------||"
 echo -e " $Green     ||                                                                   ||"
-echo -e " $Green     ||             $Purple==>$Yellow[1️⃣] Insta information gathering$Green                    ||"
-echo -e " $Green     ||             $Purple==>️$Yellow[2️⃣] Insta brute force attack$Green                       ||"
-echo -e " $Green     ||             $Purple==>$Yellow[3️⃣] Insta auto reporter$Green                            ||"
-echo -e " $Green     ||             $Purple==>$Yellow[4️⃣] Insta phishing page(coming soon)$Green               ||"
-echo -e " $Green     ||             $Purple==>$Yellow[5️⃣] Update script$Green                                  ||"
-echo -e " $Green     ||             $Purple==>$Yellow[6️⃣] Remove script$Green                                  ||"
-echo -e " $Green     ||             $Purple==>$Yellow[7️⃣] About$Green                                          ||"
-echo -e " $Green     ||             $Purple==>$Yellow[8️⃣] exit$Green                                           ||"
+echo -e " $Green     || $Purple==>$Yellow[1️⃣] Insta information gathering$Green           ||"
+echo -e " $Green     || $Purple==>️$Yellow[2️⃣] Insta brute force attack$Green              ||"
+echo -e " $Green     || $Purple==>$Yellow[3️⃣] Insta auto reporter$Green                   ||"
+echo -e " $Green     || $Purple==>$Yellow[4️⃣] Insta phishing page(coming soon)$Green      ||"
+echo -e " $Green     || $Purple==>$Yellow[5️⃣] Update script$Green                         ||"
+echo -e " $Green     || $Purple==>$Yellow[6️⃣] Remove script$Green                         ||"
+echo -e " $Green     || $Purple==>$Yellow[7️⃣] About$Green                                 ||"
+echo -e " $Green     || $Purple==>$Yellow[8️⃣] exit$Green                                  ||"
 echo -e " $Green     ||                                                                   ||"                                                                                       
-echo -e " $Green     ||---------------------------$Cyan [select option] $Green-----------------------||"
+echo -e " $Green     ||---------------------$Cyan [select option] $Green------------------||"
 echo -e " $Green     |---------------------------------------------------------------------|"
 echo " "
 echo " "
@@ -83,7 +83,7 @@ echo " "
         echo -e "\e[1;34m Downloading Latest Files..."
         cd $HOME
         rm -rf insta-hack
-        git clone https://github.com/IncredibleHacker/insta-hack
+        git clone https://github.com/sidhaniedotcom/instahack
         cd insta-hack
         bash insta-hack.sh
      
